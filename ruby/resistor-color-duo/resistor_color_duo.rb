532696561017f55e9490ba6a1e6d7046e4f61f88
henry lay <henry88lay@gmail.com>
2020-02-21 00:21:51 +1100

    reindented the lines

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,19 +1,19 @@
 class ResistorColorDuo
 
-    RESISTOR = {
-      "black" => 0,
-      "brown" => 1,
-      "red" => 2,
-      "orange" => 3,
-      "yellow" => 4,
-      "green" => 5,
-      "blue" => 6,
-      "violet" => 7,
-      "grey" => 8,
-      "white" => 9
-    }
+  RESISTOR = {
+    "black" => 0,
+    "brown" => 1,
+    "red" => 2,
+    "orange" => 3,
+    "yellow" => 4,
+    "green" => 5,
+    "blue" => 6,
+    "violet" => 7,
+    "grey" => 8,
+    "white" => 9
+  }
 
-    def self.value(color)
-        return "#{RESISTOR[color[0]]}#{RESISTOR[color[1]]}".to_i
-    end
+  def self.value(color)
+    return "#{RESISTOR[color[0]]}#{RESISTOR[color[1]]}".to_i
+  end
 end
