1a7c34bbb2de1161391069042a5c28374b2bb6a0
henry lay <henry88lay@gmail.com>
2020-02-21 00:24:00 +1100

    cleaned out the styling errors

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,19 +1,20 @@
-class ResistorColorDuo
+# frozen_string_literal: true
 
+class ResistorColorDuo
   RESISTOR = {
-    "black" => 0,
-    "brown" => 1,
-    "red" => 2,
-    "orange" => 3,
-    "yellow" => 4,
-    "green" => 5,
-    "blue" => 6,
-    "violet" => 7,
-    "grey" => 8,
-    "white" => 9
-  }
+    'black' => 0,
+    'brown' => 1,
+    'red' => 2,
+    'orange' => 3,
+    'yellow' => 4,
+    'green' => 5,
+    'blue' => 6,
+    'violet' => 7,
+    'grey' => 8,
+    'white' => 9
+  }.freeze
 
   def self.value(color)
-    return "#{RESISTOR[color[0]]}#{RESISTOR[color[1]]}".to_i
+    "#{RESISTOR[color[0]]}#{RESISTOR[color[1]]}".to_i
   end
 end
