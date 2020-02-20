a67df69c97cd5cdb49d6bbbaedd68dbbd6c7444d
henry lay <henry88lay@gmail.com>
2020-02-20 22:08:52 +1100

    refactored the code

--- ruby/hello-world/hello_world.rb
+++ ruby/hello-world/hello_world.rb
@@ -6,4 +6,8 @@ To get started with TDD, see the `README.md` file in your
 `ruby/hello-world` directory.
 =end
 
-puts "Hello, World!"
+class HelloWorld
+  def self.hello
+    "Hello, World!"
+  end
+end
